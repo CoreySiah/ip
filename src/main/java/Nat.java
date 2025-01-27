82580ec316d14ee24b74d02c06ce340387fb9650
CoreySiah <coreysiah@gmail.com>
2025-01-27 13:42:24 +0800

    Committed Duke Level-2: Add, List

--- src/main/java/Nat.java
+++ src/main/java/Nat.java
@@ -1,22 +1,46 @@
 import java.util.Scanner;
 
 public class Nat {
-    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private String[] toDoList;
+    private int numOfItems;
+    private Scanner scanner;
+    private static final String HORIZONTAL_LINE = "    ____________________________________________________________";
     private static final String SPACER = "    ";
 
-    public static void main(String[] args) {
-        Nat natChatbot = new Nat();
-        natChatbot.printLogo();
-        natChatbot.printGreeting();
+    public Nat() {
+        this.toDoList = new String[100];
+        this.numOfItems = 0;
+        this.scanner = new Scanner(System.in);
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        Nat natCB = new Nat();
+        natCB.printLogo();
+        natCB.printGreeting();
+
+        String command = natCB.scanner.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(SPACER + " " + command);
-            System.out.println(SPACER + HORIZONTAL_LINE);
-            command = scanner.nextLine();
+            if (command.equals("list")) {
+                for (int i = 0; i < natCB.numOfItems; i++) {
+                    int index = i + 1;
+                    System.out.println(SPACER + " " + index + ". " + natCB.toDoList[i]);
+                }
+                System.out.println(HORIZONTAL_LINE);
+            } else {
+                System.out.println(SPACER + " added: " + command + "\n" + HORIZONTAL_LINE);
+                natCB.toDoList[natCB.numOfItems] = command;
+                natCB.numOfItems++;
+            }
+
+            command = natCB.scanner.nextLine();
+            System.out.println(HORIZONTAL_LINE);
         }
-        natChatbot.printGoodbye();
+
+        natCB.printGoodbye();
     }
 
     public void printLogo() {
@@ -26,17 +50,17 @@ public class Nat {
                 + SPACER + "  | |\\ \\| |     / ___ \\     | |     \n"
                 + SPACER + " _| |_\\   |_  _/ /   \\ \\_  _| |_    \n"
                 + SPACER + "|_____|\\____||____| |____||_____|   \n";
-        System.out.println(SPACER + "Hello from\n" + logo + SPACER + HORIZONTAL_LINE);
+        System.out.println(SPACER + "Hello from\n" + logo + HORIZONTAL_LINE);
     }
 
     public void printGreeting() {
         System.out.println(SPACER + " Good day! I'm Nat!\n"
                 + SPACER + " How may I assist you today?\n"
-                + SPACER + HORIZONTAL_LINE);
+                + HORIZONTAL_LINE);
     }
 
     public void printGoodbye() {
         System.out.println(SPACER + " Byebye! Come back soon!\n"
-                + SPACER + HORIZONTAL_LINE);
+                + HORIZONTAL_LINE);
     }
 }
