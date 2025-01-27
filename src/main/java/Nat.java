fd5aebd4893016309f46dd9986144568fac3a08c
CoreySiah <coreysiah@gmail.com>
2025-01-27 13:20:21 +0800

    Committed Duke Level-1: Echo

--- src/main/java/Nat.java
+++ src/main/java/Nat.java
@@ -1,31 +1,42 @@
+import java.util.Scanner;
+
 public class Nat {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private static final String SPACER = "    ";
+
     public static void main(String[] args) {
         Nat natChatbot = new Nat();
         natChatbot.printLogo();
         natChatbot.printGreeting();
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(SPACER + " " + command);
+            System.out.println(SPACER + HORIZONTAL_LINE);
+            command = scanner.nextLine();
+        }
         natChatbot.printGoodbye();
     }
 
     public void printLogo() {
-        String logo = " ____  _____       _     _________  \n"
-                + "|_   \\|_   _|     / \\   |  _   _  | \n"
-                + "  |   \\ | |      / _ \\  |_/ | | \\_| \n"
-                + "  | |\\ \\| |     / ___ \\     | |     \n"
-                + " _| |_\\   |_  _/ /   \\ \\_  _| |_    \n"
-                + "|_____|\\____||____| |____||_____|   \n"
-                + "                                    \n"
-                + "____________________________________________________________";
-        System.out.println("Hello from\n" + logo);
+        String logo = SPACER + " ____  _____       _     _________  \n"
+                + SPACER + "|_   \\|_   _|     / \\   |  _   _  | \n"
+                + SPACER + "  |   \\ | |      / _ \\  |_/ | | \\_| \n"
+                + SPACER + "  | |\\ \\| |     / ___ \\     | |     \n"
+                + SPACER + " _| |_\\   |_  _/ /   \\ \\_  _| |_    \n"
+                + SPACER + "|_____|\\____||____| |____||_____|   \n";
+        System.out.println(SPACER + "Hello from\n" + logo + SPACER + HORIZONTAL_LINE);
     }
 
     public void printGreeting() {
-        System.out.println(" Hello! I'm Nat!\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________");
+        System.out.println(SPACER + " Good day! I'm Nat!\n"
+                + SPACER + " How may I assist you today?\n"
+                + SPACER + HORIZONTAL_LINE);
     }
 
     public void printGoodbye() {
-        System.out.println(" Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        System.out.println(SPACER + " Byebye! Come back soon!\n"
+                + SPACER + HORIZONTAL_LINE);
     }
 }
