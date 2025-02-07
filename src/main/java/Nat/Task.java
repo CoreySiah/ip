642fdeecdccdfa7481c729d91ffe0c7c0a55eea0
CoreySiah <coreysiah@gmail.com>
2025-02-07 09:38:21 +0800

    Fix Tags (Tags weren't getting logged)

--- src/main/java/Nat/Task.java
+++ src/main/java/Nat/Task.java
@@ -9,32 +9,55 @@ public class Task {
         this.isDone = false;
     }
 
-    public boolean isDone() {
-        return this.isDone;
-    }
-
+    /**
+     * Return the task name; primarily used in JUnit testing
+     * @return taskName
+     */
     public String getTaskName() {
         return this.taskName;
     }
 
+    /**
+     * Return the task completeness; primarily used in JUnit testing
+     * @return isDone
+     */
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    /**
+     * Return the task completeness as a status icon string
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Mark the task as done
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Mark the task as not done
+     */
     public void unmarkAsDone() {
         this.isDone = false;
     }
 
+    /**
+     * Save format for Storage
+     */
     public String toSaveFormat() {
         // Format: "U | 1 | Read a book"
         String status = this.isDone ? "1" : "0";
         return getTaskType() + " | " + status + " | " + this.taskName;
     }
 
+    /**
+     * Return the placeholder task type
+     */
     public String getTaskType() {
         // Unknown task type
         return "U";
