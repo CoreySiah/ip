43e257c24382a2bd6bd37b6ea2e56121512b8b5d
CoreySiah <coreysiah@gmail.com>
2025-02-19 14:50:57 +0800

    Revert "Add Duke A-Assertions: Use Assertions"
    This reverts commit 1f720bb4266b575ddf5210ee0df3a563694ab635.

--- src/main/java/nat/Nat.java
+++ src/main/java/nat/Nat.java
@@ -19,9 +19,6 @@ public class Nat {
         this.storage = new Storage("src/main/java/data/data.txt");
         this.taskList = new TaskList(storage.load());
         this.ui = new Ui();
-
-        // Assertion: TaskList should never be null after initialization
-        assert this.taskList != null : "TaskList should not be null!";
     }
 
     /**
